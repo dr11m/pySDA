84ce3f04c01261c68ddfef05a44620997ca8f5b3
dr11m <drr111m@gmail.com>
2025-07-03 19:28:24 +0300

    модуль автоматизации всех проверок и подтверждений

---
+++ src/cli/__init__.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""
+CLI пакет для Steam Bot
+"""
+
+from .constants import MenuChoice, TradeMenuChoice, AutoMenuChoice, Messages, Formatting, Config
+from .menu_base import MenuItem, BaseMenu, NavigableMenu
+from .display_formatter import DisplayFormatter
+from .config_manager import ConfigManager
+from .trade_handlers import (
+    TradeActionHandler,
+    GiftAcceptHandler,
+    TradeConfirmHandler,
+    SpecificTradeHandler,
+    TradeCheckHandler
+)
+from .menus import MainMenu, TradesMenu, AutoMenu
+from .auto_manager import AutoManager
+
+__all__ = [
+    'MenuChoice',
+    'TradeMenuChoice', 
+    'AutoMenuChoice',
+    'Messages',
+    'Formatting',
+    'Config',
+    'MenuItem',
+    'BaseMenu',
+    'NavigableMenu',
+    'DisplayFormatter',
+    'ConfigManager',
+    'TradeActionHandler',
+    'GiftAcceptHandler',
+    'TradeConfirmHandler',
+    'SpecificTradeHandler',
+    'TradeCheckHandler',
+    'MainMenu',
+    'TradesMenu',
+    'AutoMenu',
+    'AutoManager'
+] 
